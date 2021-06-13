1816ae3a7bf6979aa8cd346db6229638ed2d1610
AdamWang518 <dodoga518@gmail.com>
2021-06-13 21:23:51 +0800

    login

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Grid, TextField, Button, Container } from "@material-ui/core";
+import { Grid, TextField, Button } from "@material-ui/core";
 import $ from "jquery";
 const Gallery = () => {
   const [keyWord, setKeyWord] = useState("");
@@ -12,13 +12,7 @@ const Gallery = () => {
       keyWord +
       "&tagmode=any&format=json&per_page=400&jsoncallback=?";
     const data = $.getJSON(dataUrl);
-    data.done(function (msg) {
-      $.each(msg.items, function (i, item) {});
-      
-    });
-    data.fail(function (msg) {
-      console.log(msg);
-    });
+    
   };
 
   return (
@@ -38,7 +32,7 @@ const Gallery = () => {
       >
         Search
       </Button>
-      <Container></Container>
+
     </Grid>
   );
 };
